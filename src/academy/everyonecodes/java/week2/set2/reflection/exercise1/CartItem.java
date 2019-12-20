e27fd872ad23dd94c52602cebb6859166f71f50f
baloghrebecca <baloghrebecca2@gmail.com>
2019-12-20 11:55:25 +0100

    Week 2 Reflection

---
+++ src/academy/everyonecodes/java/week2/set2/reflection/exercise1/CartItem.java
@@ -0,0 +1,4 @@
+package academy.everyonecodes.java.week2.set2.reflection.exercise1;
+
+public class CartItem {
+}
---
+++ src/academy/everyonecodes/java/week2/set2/reflection/exercise2/Application.java
@@ -0,0 +1,4 @@
+package academy.everyonecodes.java.week2.set2.reflection.exercise2;
+
+public class Application {
+}
